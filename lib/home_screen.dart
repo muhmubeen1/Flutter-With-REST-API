273957ce7062de59bfb49562c1df0f579c9a7522
muhmubeen1 <muhmubeen1@gmail.com>
2024-07-19 11:34:21 +0500

    intialize repo

---
+++ lib/home_screen.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_project/models/post_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body.toString());
+      postList = data
+          .map((json) => PostModel.fromJson(json as Map<String, dynamic>))
+          .toList();
+      return postList;
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Screen"),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: FutureBuilder<List<PostModel>>(
+          future: getPostApi(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData) {
+              return const Center(child: Text("No posts available"));
+            } else {
+              return ListView.builder(
+                itemCount: postList.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(postList[index].body.toString()),
+                      ],
+                    )),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
