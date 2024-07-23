725df0b726d20f2c02502aeb6a3b91dc774469ef
muhmubeen1 <muhmubeen1@gmail.com>
2024-07-23 20:58:16 +0500

    origin

--- lib/complexJson.dart
+++ lib/complexJson.dart
@@ -1,11 +1,11 @@
-// ignore_for_file: unused_local_variable
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:my_project/models/user_model/user_model.dart';
-import "package:http/http.dart" as http;
 
 class ExampleThree extends StatefulWidget {
-  const ExampleThree({super.key});
+  const ExampleThree({Key? key}) : super(key: key);
   @override
   State<ExampleThree> createState() => _ExampleThreeState();
 }
@@ -13,9 +13,18 @@ class ExampleThree extends StatefulWidget {
 class _ExampleThreeState extends State<ExampleThree> {
   List<UserModel> userList = [];
 
-  Future<List<UserModel> getUsersApi() async{
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
-  var data = jsonDecode(response.body.toString());
+  Future<List<UserModel>> getUsersApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+    List<dynamic> data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      userList = data
+          .map((json) => UserModel.fromJson(json as Map<String, dynamic>))
+          .toList();
+      return userList;
+    } else {
+      throw Exception('Failed to load users');
+    }
   }
 
   @override
@@ -26,11 +35,36 @@ class _ExampleThreeState extends State<ExampleThree> {
         appBar: AppBar(
           title: const Text("User API"),
           centerTitle: true,
-          backgroundColor: Colors.tealAccent,
+          backgroundColor: Colors.teal,
         ),
-        body: const SafeArea(
+        body: SafeArea(
             child: Column(
-          children: [],
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                  future: getUsersApi(),
+                  builder: (context, AsyncSnapshot<List<UserModel>> snapshot) {
+                    return ListView.builder(
+                        itemCount: userList.length,
+                        itemBuilder: (context, index) {
+                          if (!snapshot.hasData) {
+                            return const CircularProgressIndicator();
+                          } else {
+                            return Card(
+                              child: Column(children: [
+                                Row(
+                                  children: [
+                                    const Text("Name"),
+                                    Text(userList[index].name.toString()),
+                                  ],
+                                )
+                              ]),
+                            );
+                          }
+                        });
+                  }),
+            ),
+          ],
         )),
       ),
     );
