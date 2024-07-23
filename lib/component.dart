5399a40c762db0a3080bc5c26d698a1503f6cb32
muhmubeen1 <muhmubeen1@gmail.com>
2024-07-23 22:49:46 +0500

    handle without using model

---
+++ lib/component.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class ReuseableRow extends StatefulWidget {
+  String title, value;
+
+  ReuseableRow({Key? key, required this.title, required this.value})
+      : super(key: key);
+  @override
+  State<ReuseableRow> createState() => _ReuseableRowState();
+}
+
+class _ReuseableRowState extends State<ReuseableRow> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(widget.title),
+        const SizedBox(width: 5),
+        Text(widget.value),
+      ],
+    );
+  }
+}
---
+++ lib/exampleFour.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'component.dart';
+
+class Examplefour extends StatefulWidget {
+  const Examplefour({super.key});
+  @override
+  State<Examplefour> createState() => _ExamplefourState();
+}
+
+class _ExamplefourState extends State<Examplefour> {
+  var data;
+  Future<void> getData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("User x Models"),
+          backgroundColor: Colors.limeAccent,
+        ),
+        body: SafeArea(
+            child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                  future: getData(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Column(
+                            children: [
+                              ReuseableRow(
+                                  title: 'Name', value: data[index]['name']),
+                              ReuseableRow(
+                                  title: 'Email', value: data[index]['email']),
+                              ReuseableRow(
+                                  title: 'Address',
+                                  value: data[index]['address']['city']),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
